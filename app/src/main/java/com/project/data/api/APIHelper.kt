10932f6b8594e0e6ef6edbab0841b13e6e4a800b
Dong-Han <donghk77@naver.com>
2024-12-16 18:26:59 +0900

    feat : add apihelper

--- app/src/main/java/com/project/data/api/APIHelper.kt
+++ app/src/main/java/com/project/data/api/APIHelper.kt
@@ -1,4 +1,13 @@
 package com.project.data.api
 
-class APIHelper {
+import com.project.data.models.Login
+import com.project.data.models.UserCreate
+
+class ApiHelper(private val accountsService: AccountsService) {
+
+    suspend fun login(request: Login) = accountsService.loginRequest(request)
+
+    suspend fun logout() = accountsService.logoutRequest()
+
+    suspend fun userCreate(request: UserCreate) = accountsService.userCreateRequest(request)
 }
